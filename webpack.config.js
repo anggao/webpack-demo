fb3717794740978a11005879a7d1b8bb66d902c3
Ang Gao <ang.gao87@gmail.com>
2016-02-29 01:26:13 +0000

    create multiple bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 var path = require('path');
+var webpack = require('webpack');
 
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
   context: path.resolve('js'),
-  entry: ["./utils.js", "./app"],
+  entry: {
+    about: './about_page.js',
+    home: './home_page.js',
+    contact: './contact_page.js'
+  },
   output: {
     path: path.resolve('build/js/'),
     publicPath: '/public/assets/js/',
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   watch: true,
   devServer: {
     contentBase: 'public'
   },
+
+  plugins: [
+    commonsPlugin
+  ],
   module: {
     preloaders: [
       {
